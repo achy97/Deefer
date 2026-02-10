30653a35fee4d78dce435d42d124cbd0b02053c3
achy97 <achy97405@gmail.com>
2026-02-10 12:22:52 +0600

    Server.js
    Add new file

---
+++ Server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("file", (file) => {
+    socket.broadcast.emit("file", file);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
